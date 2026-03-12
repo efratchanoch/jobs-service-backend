665d73da8d3d6e420c497cd495cf665859c20163
efratchanoch <efratchanoch@gmail.com>
2026-03-12 18:42:56 +0200

    Implement Generic Repository pattern and HealthCheck

---
+++ Data/Enums/Field.cs
@@ -0,0 +1,9 @@
+namespace jobs_service_backend.Data.Enums;
+
+public enum Field
+{
+    Development,
+    QA,
+    DevOps
+}
+
---
+++ Data/Enums/JobType.cs
@@ -0,0 +1,9 @@
+namespace jobs_service_backend.Data.Enums;
+
+public enum JobType
+{
+    FullTime,
+    PartTime,
+    Internship
+}
+
